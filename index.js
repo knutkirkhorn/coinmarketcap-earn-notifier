cc25a5c649159c756337ddac10aa5493a561cef3
Knut Kirkhorn <knutkirk@hotmail.com>
2022-06-13 19:14:54 +0200

    Use top-level await

--- index.js
+++ index.js
@@ -27,32 +27,30 @@ async function notifyNewCampaign(campaign) {
     });
 }
 
-(async () => {
-    let activeCampaigns = await fetchActiveCampaigns();
+let activeCampaigns = await fetchActiveCampaigns();
 
-    // Make it run forever
-    while (true) {
-        try {
-            console.log('Checking for earn campaigns at:', new Date());
+// Make it run forever
+while (true) {
+    try {
+        console.log('Checking for earn campaigns at:', new Date());
 
-            const currentActiveCampaigns = await fetchActiveCampaigns();
-            // eslint-disable-next-line no-loop-func
-            const newActiveCampaigns = currentActiveCampaigns.filter(campaign => !activeCampaigns.some(activeCampaign => activeCampaign.name === campaign.name));
+        const currentActiveCampaigns = await fetchActiveCampaigns();
+        // eslint-disable-next-line no-loop-func
+        const newActiveCampaigns = currentActiveCampaigns.filter(campaign => !activeCampaigns.some(activeCampaign => activeCampaign.name === campaign.name));
 
-            if (activeCampaigns.length === currentActiveCampaigns.length && newActiveCampaigns.length === 0) {
-                // eslint-disable-next-line no-continue
-                continue;
-            }
-
-            activeCampaigns = currentActiveCampaigns;
-
-            for (let i = 0; i < newActiveCampaigns.length; i++) {
-                await notifyNewCampaign(newActiveCampaigns[i]);
-            }
-        } catch (error) {
-            console.log(error);
-        } finally {
-            await setTimeout(config.waitTimeout);
+        if (activeCampaigns.length === currentActiveCampaigns.length && newActiveCampaigns.length === 0) {
+            // eslint-disable-next-line no-continue
+            continue;
         }
+
+        activeCampaigns = currentActiveCampaigns;
+
+        for (let i = 0; i < newActiveCampaigns.length; i++) {
+            await notifyNewCampaign(newActiveCampaigns[i]);
+        }
+    } catch (error) {
+        console.log(error);
+    } finally {
+        await setTimeout(config.waitTimeout);
     }
-})();
+}
